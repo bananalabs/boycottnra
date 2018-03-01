4c6d5126cc046e6ba32fc94b8c1f663574b59d7c
dramam <dshenoy@gmail.com>
2018-03-01 10:41:23 -0800

    only a single checkbox should be active at a time for a site

--- src/scripts/sites.js
+++ src/scripts/sites.js
@@ -5,78 +5,92 @@ import guid from './utils/browserfingerprint.js';
 
 const id = guid();
 
-function blockSite(event) {
+function blockSite(event, list) {
+    console.log('blockSite');
+    console.log(event);
+    console.log(list);
     const domain = event.target.id.split('_')[1];
-    getBlackWhiteList()
-    .then((snapshot) => {
-        let list = snapshot.child(id).val() || {};
-        event.target.checked ?
-        blockDomain(list, id, domain, true, false) :
+    if (event.target.checked) {
+        blockDomain(list, id, domain, true, false);
+        // Uncheck Allow checkbox and remove domain from whitelist
+        const allowCheckbox = document.getElementById(`allow_`+domain);
+        allowCheckbox.checked = false;
+    } else {
         unblockDomain(list, id, domain);
-    })
-    .catch((err) => console.log(err));
+    }
+    
 }
 
-function allowSite(event) {
+function allowSite(event, list) {
     const domain = event.target.id.split('_')[1];
-    getBlackWhiteList()
-    .then((snapshot) => {
-        let list = snapshot.child(id).val() || {};
-        event.target.checked ?
-        allowDomain(list, id, domain) :
+    if (event.target.checked) {
+        allowDomain(list, id, domain);
+        // Uncheck Block checkbox and remove domain from blacklist
+        const blockCheckbox = document.getElementById(`block_`+domain);
+        blockCheckbox.checked = false;
+    } else {
         unallowDomain(list, id, domain);
-    });
+    }
 }
 
 (function createSitesTable() {
-    const table = document.getElementById('sites_table');
-    // Create a row for each site that supports NRA
-    Object.keys(sites).forEach((siteName) => {
-        if (sites[siteName] === 'supportsNRA') {
-            let row = addElement(
-                "tr",
-                {
-                    id: `row_+${siteName}`
-                },
-                table
-            );
-            let colName = addElement(
-                "td",
-                {
-                    innerHTML: siteName
-                },
-                row
-            );
-            let colBlock = addElement(
-                "td",
-                {},
-                row
-            );
-            let colBlockContent = addElement(
-                "input",
-                {
-                    type: "checkbox",
-                    name: `block_${siteName}`,
-                    id: `block_${siteName}`
-                },
-                colBlock
-            );
-            colBlockContent.addEventListener("change", blockSite);
-            let colAllow = addElement(
-                "td",
-                {},
-                row
-            );
-            let colAllowContent = addElement(
-                "input",
-                {
-                    type: "checkbox",
-                    name: `allow_${siteName}`,
-                    id: `allow_${siteName}`
-                },
-                colAllow
-            );
-            colAllowContent.addEventListener("change", allowSite);
-        }
+    getBlackWhiteList()
+    .then((snapshot) => {
+        let list = snapshot.child(id).val() || {};
+        const blacklist = list['blacklist'] || [];
+        const whitelist = list['whitelist'] || [];
+        const table = document.getElementById('sites_table');
+        // Create a row for each site that supports NRA
+        Object.keys(sites).forEach((siteName) => {
+            if (sites[siteName] === 'supportsNRA') {
+                let row = addElement(
+                    "tr",
+                    {
+                        id: `row_+${siteName}`
+                    },
+                    table
+                );
+                let colName = addElement(
+                    "td",
+                    {
+                        innerHTML: siteName
+                    },
+                    row
+                );
+                let colBlock = addElement(
+                    "td",
+                    {},
+                    row
+                );
+                let colBlockContent = addElement(
+                    "input",
+                    {
+                        type: "checkbox",
+                        name: `block_${siteName}`,
+                        id: `block_${siteName}`,
+                        checked: blacklist.includes(siteName)
+                    },
+                    colBlock
+                );
+                colBlockContent.addEventListener("change", (event) => blockSite(event, list));
+                let colAllow = addElement(
+                    "td",
+                    {},
+                    row
+                );
+                let colAllowContent = addElement(
+                    "input",
+                    {
+                        type: "checkbox",
+                        name: `allow_${siteName}`,
+                        id: `allow_${siteName}`,
+                        checked: whitelist.includes(siteName)
+                    },
+                    colAllow
+                );
+                colAllowContent.addEventListener("change", (event) => allowSite(event, list));
+            }
+        })
     })
+    .catch((err) => console.log(err));
 })();
